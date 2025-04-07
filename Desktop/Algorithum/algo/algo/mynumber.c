d409814ccdb75fd3fba88485ae9f1e3a5999b5d4
kya11 <hsk123823@gmail.com>
2025-04-08 02:28:21 +0900

    Add even number

--- Desktop/Algorithum/algo/algo/mynumber.c
+++ Desktop/Algorithum/algo/algo/mynumber.c
@@ -6,5 +6,12 @@ int main() {
             printf("%d\n", i);
         }
     }
+
+    for (int i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+            printf("%d\n", i);
+        }
+    }
+
     return 0;
 }
