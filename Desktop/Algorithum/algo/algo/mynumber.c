4591e35a3939aad393bc501ecc4b8dd51d081e19
kya11 <hsk123823@gmail.com>
2025-04-08 02:33:21 +0900

    Add prime number

--- Desktop/Algorithum/algo/algo/mynumber.c
+++ Desktop/Algorithum/algo/algo/mynumber.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+int is_prime(int num) {
+    if (num < 2) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
 int main() {
     for (int i = 1; i <= 100; i++) {
         if (i % 2 == 1) {
@@ -12,6 +18,10 @@ int main() {
             printf("%d\n", i);
         }
     }
-
+    for (int i = 1; i <= 100; i++) {
+        if (is_prime(i)) {
+            printf("%d\n", i);
+        }
+    }
     return 0;
 }
